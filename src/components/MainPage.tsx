51658af0640fbe4da845d793fbfef04ff08e5ad2
Jagashira <pyprogjas.3104@gmail.com>
2023-03-02 19:47:59 +0900

    feat(main page right) メインの右側の基盤完成

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import styles from "../styles/MainPage.module.css";
+import LeftContent from "./LeftContent";
+import RIghtContent from "./RIghtContent";
 
 const MainPage = () => {
-  return <div>MainPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <LeftContent />
+      </div>
+      <div className={styles.right}>
+        <RIghtContent />
+      </div>
+    </div>
+  );
 };
 
 export default MainPage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import MainPage from "@/components/MainPage";
 import Head from "next/head";
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/icon.jpg" />
       </Head>
+      <MainPage />
     </>
   );
 }
