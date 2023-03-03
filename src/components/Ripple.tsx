8c0f4a93a11a8e437e4ee78028c17ed432ace1e1
Jagashira <pyprogjas.3104@gmail.com>
2023-03-04 01:33:36 +0900

    learn(add move of points)

--- src/components/Ripple.tsx
+++ src/components/Ripple.tsx
@@ -1,30 +1,63 @@
-import React, { Suspense, useMemo } from "react";
+import React, { Suspense, useCallback, useMemo, useRef } from "react";
 import * as THREE from "three";
-import { Canvas, useLoader } from "react-three-fiber";
+import { Canvas, useFrame, useLoader } from "react-three-fiber";
 import circleImg from "public/img/circle.png";
 import styles from "../styles/Ripple.module.css";
 
 const Points = () => {
   const imgTex = useLoader(THREE.TextureLoader, "/img/circle.png");
+  const bufferRef = useRef();
+
   const count = 100;
   const sep = 3;
+  let t = 0;
+  let f = 0.002;
+  let a = 3;
+  const graph = useCallback(
+    (x: number, z: number) => {
+      return Math.sin(f * (x ** 2 + z ** 2 + t)) * a;
+    },
+    [t, f, a]
+  );
   let positions = useMemo(() => {
     let positions = [];
+
     for (let xi = 0; xi < count; xi++) {
       for (let zi = 0; zi < count; zi++) {
         let x = sep * (xi - count / 2);
         let z = sep * (zi - count / 2);
-        let y = 0;
+        let y = graph(x, z);
         positions.push(x, y, z);
       }
     }
 
     return new Float32Array(positions);
-  }, [count, sep]);
+  }, [count, sep, graph]);
+
+  useFrame(() => {
+    t += 15;
+
+    const positions = bufferRef.current.array;
+
+    let i = 0;
+    for (let xi = 0; xi < count; xi++) {
+      for (let zi = 0; zi < count; zi++) {
+        let x = sep * (xi - count / 2);
+        let z = sep * (zi - count / 2);
+
+        positions[i + 1] = graph(x, z);
+        i += 3;
+      }
+    }
+
+    bufferRef.current.needsUpdate = true;
+  });
+
   return (
     <points>
       <bufferGeometry attach="geometry">
         <bufferAttribute
+          ref={bufferRef}
           attach="attributes-position"
           array={positions}
           itemSize={3}
