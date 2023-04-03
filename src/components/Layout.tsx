1b5ccccce079aa44711745ce3df53dd56db884f5
Jagashira <pyprogjas.3104@gmail.com>
2023-04-03 20:26:37 +0900

    fix(overflowX:"hidden")

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -14,7 +14,6 @@ const Layout = ({ children }: Props) => {
           position: "fixed",
           zIndex: 100,
           width: "100vw",
-          overflowX: "hidden",
         }}
       >
         <Navbar />
