c4ec12f88a85d3f78facf1633a6a62179abdf82b
Jagashira <pyprogjas.3104@gmail.com>
2023-04-03 23:54:12 +0900

    check

--- src/pages/activity.tsx
+++ src/pages/activity.tsx
@@ -21,7 +21,9 @@ const Activity = () => {
       <div className="flex-1">
         <ActivityTime />
       </div>
-      <div className="flex-1">{/* <CalendarContainer /> */}</div>
+      <div className="flex-1">
+        <CalendarContainer />
+      </div>
       <div className="flex-1">{/* <Feedback /> */}</div>
     </>
   );
