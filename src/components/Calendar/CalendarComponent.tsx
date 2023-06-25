494e800334668427869dfeac5816dc0a429a9ed9
Jagashira <pyprogjas.3104@gmail.com>
2023-06-26 05:15:24 +0900

    feat(add fullcalendar) cssを整える

---
+++ src/components/Calendar/CalendarComponent.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import FullCalendar from '@fullcalendar/react' // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import timeGridPlugin from '@fullcalendar/timegrid';
+import googleCalendarPlugin from '@fullcalendar/google-calendar';
+import listPlugin from '@fullcalendar/list';
+import styled from 'styled-components';
+const CalendarComponent= () => {
+  const CalendarStyled = styled.div`
+  background-color: white;
+  border-radius: 40px;
+  display: flex;
+  flex-direction: column;
+  width: 80vw;
+  padding: 3vh;
+  margin:auto;
+  overflow: hidden;
+
+  @media screen and (max-width: 768px){
+    transform: scale(90%);
+    
+  }
+`;
+  return (
+    <CalendarStyled>
+      <div>
+        <FullCalendar
+        plugins={[timeGridPlugin, googleCalendarPlugin,dayGridPlugin,listPlugin]}
+        initialView="dayGridMonth"
+        googleCalendarApiKey= "AIzaSyDYjgMkDJlR0dFXJ5pWw6p2l56j5TrOyFE"
+        eventSources={[
+            { googleCalendarId: "873ed3ec6ebd0884b08819ed82d23e25ff9951d8d2c48090031e53e9891313c3@group.calendar.google.com" }
+          ]}
+      />
+    </div>
+    </CalendarStyled>
+  )
+}
+
+export default CalendarComponent
