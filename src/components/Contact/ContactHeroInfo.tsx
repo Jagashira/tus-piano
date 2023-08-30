5ef1126b1bc81ca84a6f01bd12d1e4385a8c0f7b
jagashira <pyprogjas.3104@gmail.com>
2023-08-30 22:47:46 +0900

    feat(/contact add infomation)

---
+++ src/components/Contact/ContactHeroInfo.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "@/styles/Contact.module.css";
+import { Icon } from "@iconify/react";
+
+interface Props {
+  info: {
+    id: number;
+    icon: string;
+    title: string;
+    content: string;
+  };
+}
+const ContactHeroInfo = ({ info }: Props) => {
+  return (
+    <div className={styles.gmailContainer}>
+      <div className={styles.gmail}>
+        <Icon icon={info.icon} />
+        <p>{info.title}</p>
+      </div>
+      <p>{info.content}</p>
+    </div>
+  );
+};
+
+export default ContactHeroInfo;
--- src/components/ContactHero.tsx
+++ src/components/ContactHero.tsx
@@ -2,8 +2,29 @@ import React from "react";
 import Image from "next/image";
 import styles from "@/styles/Contact.module.css";
 import { Icon } from "@iconify/react";
+import ContactHeroInfo from "./Contact/ContactHeroInfo";
 
 const ContactHero = () => {
+  const info = [
+    {
+      id: 1,
+      icon: "mdi:gmail",
+      title: "Gmail",
+      content: "example@gmail.com",
+    },
+    {
+      id: 2,
+      icon: "mdi:phone",
+      title: "Phone number",
+      content: "080-0000-0000",
+    },
+    {
+      id: 3,
+      icon: "mdi:map-marker",
+      title: "Address",
+      content: "〒278-8510",
+    },
+  ];
   return (
     <>
       <div className={styles.contactHero}>
@@ -15,11 +36,9 @@ const ContactHero = () => {
           className={styles.contactImg}
         />
         <div className={styles.heroDetail}>
-          <div className={styles.gmail}>
-            <Icon icon="mdi:gmail" />
-            <p>Gmail</p>
-          </div>
-          <p>example@gmail.com</p>
+          {info.map((item) => {
+            return <ContactHeroInfo key={item.id} info={item} />;
+          })}
         </div>
       </div>
     </>
